eecc28b1d2ae2df20bd31d8f75182784361e7320
Saher Patait <saherpatait15@gmail.com>
2023-01-30 13:33:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,3 @@ bill_per=total/people
 final_Amt=round(bill_per,3)
 print(f"Each person should pay: ${final_Amt}")
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
-
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
